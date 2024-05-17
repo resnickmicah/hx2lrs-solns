e840ccc1f078a9f7d3e7a4d944c4abc7579128cb
Micah Resnick <mresnick7@gatech.edu>
2024-05-16 22:18:08 -0700

    Finish ex02_basic_calculator

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut n = n;
+    let mut product = 1;
+    while(n > 0) {
+        product *= n;
+        n -= 1;
+    }
+    product
 }
 
 #[cfg(test)]
