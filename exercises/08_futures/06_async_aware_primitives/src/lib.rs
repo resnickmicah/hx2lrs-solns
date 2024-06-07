f4c43208a0a517d852154384a36d83103d055fb6
Micah Resnick <mresnick7@gatech.edu>
2024-06-07 07:52:59 -0700

    Solve ex08/06_async_aware_primitives

--- exercises/08_futures/06_async_aware_primitives/src/lib.rs
+++ exercises/08_futures/06_async_aware_primitives/src/lib.rs
@@ -4,25 +4,28 @@
 ///  the testing code too, yes).
 ///
 /// Can you understand the sequence of events that can lead to a deadlock?
-use std::sync::mpsc;
+use tokio::sync::mpsc;
 
 pub struct Message {
     payload: String,
     response_channel: mpsc::Sender<Message>,
 }
 
+const BUFFER_SIZE: usize = 4;
+
 /// Replies with `pong` to any message it receives, setting up a new
 /// channel to continue communicating with the caller.
 pub async fn pong(mut receiver: mpsc::Receiver<Message>) {
     loop {
-        if let Ok(msg) = receiver.recv() {
+        if let Some(msg) = receiver.recv().await {
             println!("Pong received: {}", msg.payload);
-            let (sender, new_receiver) = mpsc::channel();
+            let (sender, new_receiver) = mpsc::channel(BUFFER_SIZE);
             msg.response_channel
                 .send(Message {
                     payload: "pong".into(),
                     response_channel: sender,
                 })
+                .await
                 .unwrap();
             receiver = new_receiver;
         }
@@ -31,23 +34,24 @@ pub async fn pong(mut receiver: mpsc::Receiver<Message>) {
 
 #[cfg(test)]
 mod tests {
-    use crate::{pong, Message};
-    use std::sync::mpsc;
+    use crate::{pong, Message, BUFFER_SIZE};
+    use tokio::sync::mpsc;
 
     #[tokio::test]
     async fn ping() {
-        let (sender, receiver) = mpsc::channel();
-        let (response_sender, response_receiver) = mpsc::channel();
+        let (sender, receiver) = mpsc::channel(BUFFER_SIZE);
+        let (response_sender, mut response_receiver) = mpsc::channel(BUFFER_SIZE);
         sender
             .send(Message {
                 payload: "pong".into(),
                 response_channel: response_sender,
             })
+            .await
             .unwrap();
 
         tokio::spawn(pong(receiver));
 
-        let answer = response_receiver.recv().unwrap().payload;
+        let answer = response_receiver.recv().await.unwrap().payload;
         assert_eq!(answer, "pong");
     }
 }
